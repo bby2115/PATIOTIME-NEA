01e2c436cc21b73d5a5eff8393e08275e73fe72f
bby2115 <abiketa19@gmail.com>
2026-05-23 12:59:41 +0200

    Fix database connection with DATABASE_URL

--- db_connect.php
+++ db_connect.php
@@ -5,7 +5,17 @@ if (!$database_url) {
     die("DATABASE_URL nuk eshte vendosur ne Render.");
 }
 
-$conn = pg_connect($database_url);
+$db = parse_url($database_url);
+
+$host = $db["host"];
+$port = $db["port"];
+$user = $db["user"];
+$password = $db["pass"];
+$dbname = ltrim($db["path"], "/");
+
+$conn_string = "host=$host port=$port dbname=$dbname user=$user password=$password sslmode=require";
+
+$conn = pg_connect($conn_string);
 
 if (!$conn) {
     die("Lidhja me databazen deshtoi.");
