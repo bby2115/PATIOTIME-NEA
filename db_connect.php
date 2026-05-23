2a43e8c8ced673e855baa2968f1706654d36b2bc
bby2115 <abiketa19@gmail.com>
2026-05-23 02:26:16 +0200

    Add files via upload

--- db_connect.php
+++ db_connect.php
@@ -1,16 +1,13 @@
 <?php
-$host = "aws-1-eu-central-1.pooler.supabase.com";
-$port = "5432";
-$dbname = "postgres";
-$user = "postgres.antfyiwtglchnaoxfmod";
-$password = "Patiotime123!";
+$host = getenv("DB_HOST");
+$port = getenv("DB_PORT");
+$dbname = getenv("DB_NAME");
+$user = getenv("DB_USER");
+$password = getenv("DB_PASSWORD");
 
 $conn = pg_connect("host=$host port=$port dbname=$dbname user=$user password=$password sslmode=require");
 
 if (!$conn) {
-    $error = error_get_last();
-    die("Lidhja deshtoi: " . $error['message']);
+    die("Lidhja me databazen deshtoi.");
 }
-
-echo "Lidhja u krye me sukses!";
 ?>
